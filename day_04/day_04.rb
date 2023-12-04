720cf83b8b8dfa997c37d981b4aa23a32cd35840
MattCrl <matth.carlevaris@gmail.com>
2023-12-04 13:48:06 +0100

    Day 04 Part 1 done

--- day_04/day_04.rb
+++ day_04/day_04.rb
@@ -10,7 +10,29 @@ module Day_04
     file_data.split("\n")
   end
 
+  # Part 1
   def scratchcards_points_calculator(input)
     return nil if input.nil?
+    result = []
+    input.map{|line|
+  
+      formated = line.scan(/: .+/).first.gsub(': ', '').split('|').map{_1.strip}
+      winning_numbers = formated[0].split.map &:to_i
+      numbers_i_have = formated[1].split.map &:to_i
+      common_values = winning_numbers & numbers_i_have
+      result << (2**common_values.length)/2
+    }
+    result.sum
   end
+
+  def solve_part_one
+    scratchcards_points_calculator(read_input_file(INPUT_FILENAME))
+  end
+
+  # Part 2
+  def solve_part_two
+    nil
+  end
+
 end
+
--- spec/day_04/day_04_spec.rb
+++ spec/day_04/day_04_spec.rb
@@ -2,12 +2,12 @@ require_relative "../../day_04/day_04"
 RSpec.describe Day_04 do
   it "test scratchcards_points_calculator with samples" do
     file_data = Day_04.read_input_file("spec/day_04/day_04_samples.txt")
-    expect(Day_04.scratchcards_points_calculator(file_data)).to eq(nil)
+    expect(Day_04.scratchcards_points_calculator(file_data)).to eq(13)
   end
 
   it "test scratchcards_points_calculator with final inputs" do
     file_data = Day_04.read_input_file("spec/day_04/day_04_input.txt")
-    expect(Day_04.scratchcards_points_calculator(file_data)).to eq(nil)
+    expect(Day_04.scratchcards_points_calculator(file_data)).to eq(21088)
   end
 
 end
