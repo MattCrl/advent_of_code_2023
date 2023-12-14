9eef297ce18151fc2246cf8d14d88b9647022b47
MattCrl <carlevaris@keensaas.fr>
2023-12-14 21:31:37 +0100

    Day 9 part 2 done

--- src/day_09/day_09.rb
+++ src/day_09/day_09.rb
@@ -20,11 +20,29 @@ module Day_09
 
   def subtract_consecutive_elements(array)
     result = [array]
-    while array.length > 1
+    while array.length > 1 
       array = array.each_cons(2).map { |a, b| b - a }
       result << array
     end
     result
+  end
+
+  def part_two
+    data = File.read(INPUT_FILENAME).split("\n")
+    total = 0
+    data.map { |line|
+      my_arr = []
+      my_arr << line.split.map(&:to_i)
+      all_arr = subtract_consecutive_elements(my_arr[0])
+      all_arr.each_with_index do |current_arr, i|
+        if i.even?
+          total += current_arr[0]
+        else
+          total -= current_arr[0]
+        end
+      end
+    }
+    puts total
   end
 end
 
--- src/day_09/day_09_solvers.rb
+++ src/day_09/day_09_solvers.rb
@@ -1,4 +1,4 @@
 require_relative 'day_09'
 
 puts Day_09.part_one
-#puts Day_09.part_two
+puts Day_09.part_two
